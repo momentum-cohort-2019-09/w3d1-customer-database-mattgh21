2024df2a698dff0ae2a4e72c94d756f8586f9871
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-09-23 21:43:05 -0400

    adds formatting for address with abbreviated state name

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ function customerToHtml(customer) {
         <div class="customer_name ">${capitalize(customer.name.first)} ${capitalize(customer.name.last)}</div>
         <div class="customer_email">${customer.email}</div>
         <div class="customer_info">
-        <div class="customer_address">${customer.location.street}</div>
+        <div class="customer_address">${customer.location.street + ". " + customer.location.city + ", " + nameToAbbr(customer.location.state) + " " + customer.location.postcode}</div>
         <div class="customer_phone">${customer.phone}</div>
         <div class="customer_dob">DOB: ${moment(customer.dob).format('MMM do YYYY')}</div>
         <div class="customer_registered">Customer Since: ${moment(customer.registered).format('MMM do YYYY')}</div>
